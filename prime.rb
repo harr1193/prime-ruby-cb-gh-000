5ff5107953cdcdaa9095739a0d0e5a4d04a8eb37
Kyle Harris <k_harris95@live.com>
2018-01-22 03:17:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,10 @@
 # Add  code here!
 def prime?(num)
-  if num < 1
-    return false
-  else
-    (2..num).to_a.each { |i|
-      if num % i == 0
-        return false
-      else
-        return true
-      end
-     }
-   end
+  (2..num).to_a.each { |i|
+    if num % i == 0
+      return false
+    else
+      return true
+    end
+   }
 end
